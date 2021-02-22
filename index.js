a2167ba5de784598af1a91420313e66ed2c9ab91
Sean Purcell <me@seanp.xyz>
2021-02-21 19:27:53 -0500

    add basic splitwise and ynab stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-const sw = require('splitwise');
-const ynab = require('ynab');
+const Splitwise = require('splitwise');
+const ynabApi = require('ynab');
 const zookeeper = require('node-zookeeper-client');
 const nconf = require('nconf');
 
@@ -47,7 +47,27 @@ async function getValue(zoo, path) {
   });
 }
 
-async function main() {
+const getYnabAccount = async (ynab) => {
+  const { budgetName, accountName } = (() => {
+    const { budget, account } = nconf.get('ynab');
+    return { budgetName: budget, accountName: account };
+  })();
+  const { budgets } = (await ynab.budgets.getBudgets()).data;
+  const budget = budgets.find((budget) => budget.name == budgetName || budget.id == budgetName);
+  if (budget == undefined) {
+    throw new Error(`budget ${budgetName} not found`);
+  }
+  console.log('budget:\n%o', budget);
+  const { accounts } = (await ynab.accounts.getAccounts(budget.id)).data;
+  const account = accounts.find((account) => account.name == accountName || account.id == accountName);
+  if (account == undefined) {
+    throw new Error(`account ${accountName} not found`);
+  }
+  console.log('account:\n%o', account);
+  return { budget: budget.id, account: account.id };
+};
+
+const main = async () => {
   const zooconf = nconf.get('zoo');
   console.log('zookeeper config: %o', zooconf);
 
@@ -56,8 +76,39 @@ async function main() {
   await ensureExists(zoo, LATEST_UPDATE);
   const latest_update = await getValue(zoo, LATEST_UPDATE);
 
+  const startDate = await (async () => {
+    const startDate = nconf.get('start_date');
+    if (startDate) {
+      return startDate;
+    }
+
+    return await getValue(zoo, '/start_date');
+  })();
+  const splitwiseApiKey = await getValue(zoo, '/splitwise/api_key');
+  const ynabApiKey = await getValue(zoo, '/ynab/api_key');
+
+  console.log(`split api key: ${splitwiseApiKey}`);
+  console.log(`ynab api key: ${ynabApiKey}`);
+
   console.log(`latest update: ${latest_update}`);
-}
+
+  const sw = Splitwise({
+    logger: console.log, apiKey: splitwiseApiKey,
+  });
+
+  const ynab = new ynabApi.API(ynabApiKey);
+
+  const swUser = await sw.getCurrentUser();
+  console.log('splitwise user: %o', swUser);
+
+  const expenses = await sw.getExpenses({
+    dated_after: startDate,
+    limit: 0,
+  });
+  console.log('expenses:\n%o', expenses);
+
+  const { budget, account } = await getYnabAccount(ynab);
+};
 
 main().then(() => process.exit(0)).catch((e) => {
   console.log(`Error: ${e}`);
